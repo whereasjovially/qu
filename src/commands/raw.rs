b00e2e9c302c7c896ba5244ab3aef3030527646a
whereasjovially <whereasjovially@outlook.com>
2022-05-06 17:38:16 +0200

    use file as arg content for raw

--- src/commands/raw.rs
+++ src/commands/raw.rs
@@ -1,11 +1,12 @@
-use std::str::FromStr;
-
 use crate::lib::{
     signing::{sign_ingress_with_request_status_query, IngressWithRequestId},
     AnyhowResult,
 };
+use anyhow::anyhow;
 use candid::Principal;
 use clap::Parser;
+use std::path::PathBuf;
+use std::str::FromStr;
 
 /// Raw canister call
 #[derive(Parser)]
@@ -17,14 +18,35 @@ pub struct Opts {
     method: String,
 
     /// Method arguments as a Candid string
-    args: String,
+    args: Option<String>,
+
+    /// Binary file
+    args_file: Option<PathBuf>,
 }
 
 pub fn exec(pem: &str, opts: Opts) -> AnyhowResult<Vec<IngressWithRequestId>> {
+    let bytes = match (&opts.args, &opts.args_file) {
+        (Some(args), None) => candid::IDLArgs::from_str(&args)?.to_bytes()?,
+        (None, Some(path)) => {
+            use std::{
+                fs::File,
+                io::{BufReader, Read},
+            };
+            let mut reader = BufReader::new(File::open(path)?);
+            let mut buffer = Vec::new();
+            reader.read_to_end(&mut buffer)?;
+            buffer
+        }
+        _ => {
+            return Err(anyhow!(
+                "String args or a file with argument bytes should be specified".to_owned(),
+            ))
+        }
+    };
     Ok(vec![sign_ingress_with_request_status_query(
         pem,
         opts.canister_id,
         &opts.method,
-        candid::IDLArgs::from_str(&opts.args)?.to_bytes()?,
+        bytes,
     )?])
 }
